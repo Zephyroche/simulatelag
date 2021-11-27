4893af05119b73660822ef333a92837f7c05f8a2
Zephyroche <pvarpella11@gmail.com>
2021-11-27 21:21:15 +0700

    Add CORRUPT param

--- simulatelag.sh
+++ simulatelag.sh
@@ -4,11 +4,17 @@ TC=/sbin/tc #Default path to tc
 IF=eth #Interface name
 DST_CIDR=ip #Address to apply lag on
 
-LOSS="15%"
+LOSS="0%" #Default loss value
+CORRUPT="0%" #Default corrupt value
 
 if [ "$2" ]; then
    LOSS="$2%"
    echo "LOSS : $LOSS"
+fi
+
+if [ "$3" ]; then
+   CORRUPT="$3"
+   echo "CORRUPT : $LOSS"
 fi
 
 do_start() {
@@ -17,7 +23,7 @@ modprobe sch_netem
 
 for ETH in $IF; do
 $TC qdisc add dev $ETH root handle 1: prio
-$TC qdisc add dev $ETH parent 1:3 netem loss $LOSS
+$TC qdisc add dev $ETH parent 1:3 netem loss $LOSS corrupt $CORRUPT
 $TC filter add dev $ETH protocol ip parent 1:0 prio 3 u32 match ip dst $DST_CIDR flowid 1:3
 done;
 
@@ -52,7 +58,8 @@ case "$1" in
   do_status
    ;;
  *)
-   echo "Usage : simulatelag.sh start|stop [ \$LOSS ]"
+   echo "Usage : simulatelag.sh start|stop [ \$CORRUPT [ \$LOSS ] ]"
    echo " LOSS in %"
+   echo " CORRUPT in %"
    ;;
 esac
